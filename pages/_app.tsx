edd2672f55ba3552e6b9bfd62345efee5e2b79c1
Gilad <freeslugs@gmail.com>
2023-11-05 00:52:58 +0000

    bring back footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,56 +15,8 @@ if (!projectId) {
   throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
 }
 
-import { jsonRpcProvider } from '@wagmi/core/providers/jsonRpc'
-
- const polygonZkEvmTestnetBackup: any =  {
-  id: 1442,
-  name: 'Polygon zkEVM Testnet',
-  network: 'polygon-zkevm-testnet',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc.public.zkevm-test.net'],
-    },
-    public: {
-      http: ['https://rpc.public.zkevm-test.net'],
-    },
-  },
-  blockExplorers: {
-    blockscout: {
-      name: 'Blockscout',
-      url: 'https://explorer.public.zkevm-test.net',
-    },
-    default: {
-      name: 'PolygonScan',
-      url: 'https://testnet-zkevm.polygonscan.com',
-    },
-  },
-  testnet: true,
-  contracts: {
-    multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 525686,
-    },
-  },
-}
-
-
-// // 2. Configure Web3Modal
+ // 2. Configure Web3Modal
 const chains = [polygonZkEvmTestnet];
-// const chains: any = [
-//   jsonRpcProvider(polygonZkEvmTestnetBackup)
-// ]
-// const { chains, publicClient } = configureChains(
-//   [polygonZkEvmTestnet],
-//   [
-//     jsonRpcProvider({
-//       rpc: (chain) => ({
-//         http: `https://${chain.id}.example.com`,
-//       }),
-//     }),
-//   ],
-// )
 
 const wagmiConfig = defaultWagmiConfig({
   chains,
@@ -102,7 +54,7 @@ function MyApp({ Component, pageProps }: AppProps) {
               </Flex>
             </GridItem>
             <GridItem area={"footer"}>
-              {/* <Footer /> */}
+              <Footer />
             </GridItem>
           </Grid>
         </WagmiConfig>
