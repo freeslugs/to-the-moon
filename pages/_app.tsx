2703685ae3e4365996afe5e6cac217cef747de92
Gilad <freeslugs@gmail.com>
2023-11-05 00:34:57 +0000

    maybe remove packagel ock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider, Box, Flex, Grid, GridItem } from "@chakra-ui/react";
-import { WagmiConfig } from "wagmi";
+import { WagmiConfig, configureChains } from "wagmi";
 import { polygonZkEvmTestnet } from "wagmi/chains";
 import { theme } from "../styles/theme";
 import Footer from "../components/core/Footer";
@@ -15,8 +15,54 @@ if (!projectId) {
   throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
 }
 
-// 2. Configure Web3Modal
+// import { jsonRpcProvider } from '@wagmi/core/providers/jsonRpc'
+
+//  const polygonZkEvmTestnetBackup = /*#__PURE__*/ {
+//   id: 1442,
+//   name: 'Polygon zkEVM Testnet',
+//   network: 'polygon-zkevm-testnet',
+//   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+//   rpcUrls: {
+//     default: {
+//       http: ['https://rpc.public.zkevm-test.net'],
+//     },
+//     public: {
+//       http: ['https://rpc.public.zkevm-test.net'],
+//     },
+//   },
+//   blockExplorers: {
+//     blockscout: {
+//       name: 'Blockscout',
+//       url: 'https://explorer.public.zkevm-test.net',
+//     },
+//     default: {
+//       name: 'PolygonScan',
+//       url: 'https://testnet-zkevm.polygonscan.com',
+//     },
+//   },
+//   testnet: true,
+//   contracts: {
+//     multicall3: {
+//       address: '0xca11bde05977b3631167028862be2a173976ca11',
+//       blockCreated: 525686,
+//     },
+//   },
+// }
+
+
+// // 2. Configure Web3Modal
 const chains = [polygonZkEvmTestnet];
+// const { chains, publicClient } = configureChains(
+//   [polygonZkEvmTestnet],
+//   [
+//     jsonRpcProvider({
+//       rpc: (chain) => ({
+//         http: `https://${chain.id}.example.com`,
+//       }),
+//     }),
+//   ],
+// )
+
 const wagmiConfig = defaultWagmiConfig({
   chains,
   projectId,
